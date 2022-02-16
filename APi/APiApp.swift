c6496ad377e59db5e35cbf67e1c01217403cb098
Kemushi <tigeramano@gmail.com>
2022-02-16 10:16:47 -0600

    Initial Commit

---
+++ APi/APiApp.swift
@@ -0,0 +1,17 @@
+//
+//  APiApp.swift
+//  APi
+//
+//  Created by Student on 2/16/22.
+//
+
+import SwiftUI
+
+@main
+struct APiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
